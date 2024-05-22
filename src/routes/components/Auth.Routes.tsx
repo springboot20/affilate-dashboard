90fcf6ec64647cc06b01b25e6d5134c8d12d7941
Yunus Abbas Opeyemi <opeyemiakanbi328@gmail.com>
2024-05-22 04:31:24 +0100

    routes: Routes import from react-router-dom removed

--- src/routes/components/Auth.Routes.tsx
+++ src/routes/components/Auth.Routes.tsx
@@ -1,38 +1,36 @@
 import 'react-toastify/dist/ReactToastify.css';
-import { Routes, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import { PublicRoute, Register, Login, NotFound } from '../lazy.import';
 import AuthLayout from '../../layout/AuthLayout';
 
 function AuthRoute() {
   return (
-    <Routes>
-      <Route element={<AuthLayout />}>
-        <Route
-          path='/'
-          element={
-            <PublicRoute>
-              <Register />
-            </PublicRoute>
-          }
-        />
-        <Route
-          path='/login'
-          element={
-            <PublicRoute>
-              <Login />
-            </PublicRoute>
-          }
-        />
-        <Route
-          path='*'
-          element={
-            <PublicRoute>
-              <NotFound />
-            </PublicRoute>
-          }
-        />
-      </Route>
-    </Routes>
+    <Route element={<AuthLayout />}>
+      <Route
+        path='/'
+        element={
+          <PublicRoute>
+            <Register />
+          </PublicRoute>
+        }
+      />
+      <Route
+        path='/login'
+        element={
+          <PublicRoute>
+            <Login />
+          </PublicRoute>
+        }
+      />
+      <Route
+        path='*'
+        element={
+          <PublicRoute>
+            <NotFound />
+          </PublicRoute>
+        }
+      />
+    </Route>
   );
 }
 
