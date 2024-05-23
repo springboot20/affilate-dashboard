df46232993b5cfaebeca695a301d54921de636aa
Yunus Abbas Opeyemi <opeyemiakanbi328@gmail.com>
2024-05-23 07:12:09 +0100

    Route reconfigured

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,18 @@
-import { Suspense } from 'react';
-import { ToastContainer } from 'react-toastify';
-import { AuthProvider, useAuth } from './context/AuthContext';
-import { Loader } from './components/Loader';
+import {  useAuth } from './context/AuthContext';
 import AppRouter from './routes/components/App.Routes';
-import AppLayout from './layout/AppLayout';
+// import AppLayout from './layout/AppLayout';
 import AuthRoute from './routes/components/Auth.Routes';
-
-const DefaultApp = () => {
-  return (
-    <AuthProvider>
-      <ToastContainer />
-      <Suspense fallback={<Loader />}>
-        <AppLayout>
-          <AppRouter />
-        </AppLayout>
-      </Suspense>
-    </AuthProvider>
-  );
-};
+import { Routes } from 'react-router-dom';
 
 function App() {
   const { user, tokens } = useAuth();
 
-  if (!(user && tokens)) {
-    return <DefaultApp />;
-  } else {
-    return (
-      <Suspense fallback={<Loader />}>
-        <AuthRoute />
-      </Suspense>
-    );
-  }
+  return (
+    <Routes>
+      <AuthRoute />
+      <AppRouter />
+    </Routes>
+  );
 }
 
 export default App;
