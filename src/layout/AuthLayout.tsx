4d91d33b182d0934f0c98e8fac77c76b74904ac1
Yunus Abbas Opeyemi <opeyemiakanbi328@gmail.com>
2024-05-23 07:32:31 +0100

    Auth Layout: pathname === '/register' check changed from / to /register

--- src/layout/AuthLayout.tsx
+++ src/layout/AuthLayout.tsx
@@ -8,7 +8,7 @@ const AuthLayout = () => {
   return (
     <motion.div className='w-full flex flex-col gap-9 lg:gap-0 justify-center items-center lg:flex-row lg:justify-between lg:items-stretch h-screen flex-shrink-0'>
       <div className='hidden lg:grid w-1/2 lg:mt-0 bg-auth-bg bg-no-repeat bg-cover bg-center h-screen place-items-center'>
-        {pathname === '/' ? <RegisterAuthMessage /> : <LoginAuthMessage />}
+        {pathname === '/register' ? <RegisterAuthMessage /> : <LoginAuthMessage />}
       </div>
       <motion.div className='lg:w-3/4 lg:mt-0'>
         <Outlet />
