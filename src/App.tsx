c825e79f5e7ce7af214f67b08b1f7c1d519d3ec9
Yunus Abbas Opeyemi <opeyemiakanbi328@gmail.com>
2024-05-23 07:28:38 +0100

    file deleted and App routes reconfigure for rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,111 @@
 import { useAuth } from './context/AuthContext';
-import AppRouter from './routes/components/App.Routes';
 import AppLayout from './layout/AppLayout';
-import AuthRoute from './routes/components/Auth.Routes';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import AuthLayout from './layout/AuthLayout';
+import {
+  Login,
+  PublicRoute,
+  Register,
+  Profile,
+  Shop,
+  AddProduct,
+  ProductDescription,
+  PrivateRoute,
+  Partner,
+  Dashboard,
+} from './routes/lazy.import';
+import NotFound from './components/NotFound';
 
 function App() {
   const { user, tokens } = useAuth();
 
   return (
     <Routes>
+
       <Route
         path='/'
         element={tokens && user ? <Navigate to='/login' /> : <Navigate to='/dashboard' />}
       />
       
-      <AuthRoute />
+      <Route element={<AuthLayout />}>
+        <Route
+          path='/'
+          element={
+            <PublicRoute>
+              <Register />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path='/login'
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path='*'
+          element={
+            <PublicRoute>
+              <NotFound />
+            </PublicRoute>
+          }
+        />
+      </Route>
+
       <Route element={<AppLayout />}>
-        <AppRouter />
+        <Route
+          path='/dashboard'
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path='/add-product'
+          element={
+            <PrivateRoute>
+              <AddProduct />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path='/shop'
+          element={
+            <PrivateRoute>
+              <Shop />
+            </PrivateRoute>
+          }>
+          <Route
+            path='/shop/:id'
+            element={
+              <PrivateRoute>
+                <ProductDescription />
+              </PrivateRoute>
+            }
+          />
+        </Route>
+
+        <Route
+          path='/partner'
+          element={
+            <PrivateRoute>
+              <Partner />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path='/profile'
+          element={
+            <PrivateRoute>
+              <Profile />
+            </PrivateRoute>
+          }
+        />
       </Route>
     </Routes>
   );
